f15dfd457ed10c853041cce1ea50db509eaec7e9
MamadouAlphaBarry <m_barry@etu.enset-media.ac.ma>
2024-09-09 09:37:28 +0200

    first commit

---
+++ src/test/java/org/sid/discoveryserice/DiscoverySericeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.discoveryserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySericeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
